eeeca7b3cf0f0f20a6df0415f2ed233015627b0b
Popify <jaredtrigg99@gmail.com>
2017-05-09 19:25:22 -0400

    Finished to a functional point!

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,14 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.converter.NumberStringConverter;
+
+import java.text.DecimalFormat;
 
 public class Main extends Application
 {
@@ -28,11 +28,44 @@ public class Main extends Application
 	@Override
 	public void start(Stage window) throws Exception
 	{
-		Stage stage = new Stage();
 		TextField numberField1 = new TextField();
 		TextField numberField2 = new TextField();
 		TextField numberField3 = new TextField();
-		Button calculate = new Button();
+		Button calculate = new Button("Calculate Your Offer");
+		Label opponentStake = new Label("Opponent's Offer");
+		Label winRate = new Label("Odds");
+		Label percentEV = new Label("Minimum EV%");
+		Label myOffer = new Label("Suggested Offer");
+		TextField offer = new TextField();
+		AnchorPane mainAnchor = new AnchorPane(numberField1, numberField2, numberField3, calculate, opponentStake, winRate, percentEV, myOffer, offer);
+
+		mainAnchor.setTopAnchor(opponentStake, 5.);
+		mainAnchor.setLeftAnchor(opponentStake, 20.0);
+
+		mainAnchor.setTopAnchor(numberField1, 20.0);
+		mainAnchor.setLeftAnchor(numberField1, 20.0);
+
+		mainAnchor.setTopAnchor(winRate, 45.0);
+		mainAnchor.setLeftAnchor(winRate, 20.0);
+
+		mainAnchor.setTopAnchor(numberField2, 60.0);
+		mainAnchor.setLeftAnchor(numberField2, 20.0);
+
+		mainAnchor.setTopAnchor(percentEV, 85.0);
+		mainAnchor.setLeftAnchor(percentEV, 20.0);
+
+		mainAnchor.setTopAnchor(numberField3, 100.0);
+		mainAnchor.setLeftAnchor(numberField3, 20.0);
+
+		mainAnchor.setBottomAnchor(calculate, 20.0);
+		mainAnchor.setLeftAnchor(calculate, 20.0);
+
+		mainAnchor.setBottomAnchor(myOffer, 50.0);
+		mainAnchor.setRightAnchor(myOffer,20.0);
+
+		mainAnchor.setBottomAnchor(offer, 20.0);
+		mainAnchor.setRightAnchor(offer, 20.0);
+
 
 
 		numberField1.textProperty().addListener((observable, oldValue, newValue) ->
@@ -59,24 +92,37 @@ public class Main extends Application
 			}
 		}));
 
+		DecimalFormat shortHand = new DecimalFormat("##.##");
+
 
 
 
 		calculate.setOnAction(event ->
 		{
-			expectedValue();
+			offer.setText("");
+			double initialStake = Double.parseDouble(numberField1.getText());
+			double winOdds = Double.parseDouble(numberField2.getText());
+			double expectedValuePercent = Double.parseDouble(numberField3.getText());
+
+			double yourOffer = expectedValue(winOdds, expectedValuePercent, initialStake);
+
+			offer.appendText(shortHand.format(yourOffer));
 		} );
 
 
 
+
+
+
+
 		window.setTitle("EV Calculator");
-		window.setScene(new Scene(numberField1, 350, 200));
+		window.setScene(new Scene(mainAnchor, 350, 200));
 		window.show();
 	}
 
 	private static double expectedValue(double winRate, double minEV, double opponentStake)
 	{
-		winRate = winRate / 100;
+		winRate = winRate / 100 / 100;
 		minEV = minEV / 100;
 
 		double yourStake = (winRate * opponentStake) / (minEV + (1 - winRate));
