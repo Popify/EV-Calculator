3f46fc4557c1d695812555353617d3d877f49a9e
Popify <jaredtrigg99@gmail.com>
2017-05-11 17:54:53 -0400

    Removed unused imports.

--- src/Main.java
+++ src/Main.java
@@ -138,11 +138,8 @@ public class Main extends Application
 
 
 import java.io.IOException;
-import java.util.Observable;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
